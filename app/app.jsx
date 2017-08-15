92b0ba924340bb0c70cf37b26188062caa1aeca4
Thanawat Hirunyanitiwattana <oat_war_time@hotmail.com>
2017-08-15 11:56:50 +0700

    Add TodoApp components

--- app/app.jsx
+++ app/app.jsx
@@ -2,6 +2,8 @@ let React = require('react');
 let ReactDOM = require('react-dom');
 let {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
+let TodoApp = require('TodoApp');
+
 //Load foundation
 require('style!css!foundation-sites/dist/foundation.min.css')
 $(document).foundation();
@@ -10,6 +12,6 @@ $(document).foundation();
 require('style!css!sass!applicationStyles')
 
 ReactDOM.render(
-  <p>Boilerplate final project</p>,
+  <TodoApp/>,
   document.getElementById("app")
 );
---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,13 @@
+let React = require('react');
+
+let TodoApp = React.createClass({
+  render: function() {
+    return (
+      <div>
+        TodoApp.jsx
+      </div>
+    );
+  }
+});
+
+module.exports = TodoApp;
