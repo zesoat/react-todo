df15def3d5c32e8924e6d81e658cbd9bb424e923
Thanawat Hirunyanitiwattana <oat_war_time@hotmail.com>
2017-08-25 12:48:24 +0700

    showCompleted and searchText reducers

---
+++ app/test/reducers/reducers.test.jsx
@@ -0,0 +1,29 @@
+let expect = require('expect');
+let df = require('deep-freeze-strict');
+
+let reducers = require('reducers');
+
+describe('Reducers', () => {
+  describe('searchTextReducer', () => {
+    it('should set searchText', () => {
+      let action = {
+        type: 'SET_SEARCH_TEXT',
+        searchText: 'dog'
+      };
+      let res = reducers.searchTextReducer(df(''), df(action));
+
+      expect(res).toEqual(action.searchText);
+    });
+  });
+
+  describe('showCompletedReducer', () => {
+    it('should toggle showCompleted', () => {
+      let action = {
+        type: 'TOGGLE_SHOW_COMPLETED',
+      };
+      let res = reducers.showCompletedReducer(df(false), df(action));
+
+      expect(res).toEqual(true);
+    });
+  });
+});
